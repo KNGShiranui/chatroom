2946df1f0eba952a8db6617935fa02cf608104d9
kengo <mathematics9973@gmail.com>
2023-03-17 22:54:13 +0900

    setting of routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#new'
+  resources :users, only: %i[new create edit update show destroy]
   resources :conversations do
     resources :messages
   end
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: %i[new create destroy]
+
 end
